5eeba057d6815c62cbdbb51672b55c781c452bd4
spraveenpk077 <53965347+spraveenpk077@users.noreply.github.com>
2019-08-10 21:14:04 +0530

    Update rangeoddnumber.py

--- rangeoddnumber.py
+++ rangeoddnumber.py
@@ -1,9 +1,5 @@
 start = int(input("Enter the start of range: ")) 
-end = int(input("Enter the end of range: ")) 
-  
-# iterating each number in list 
+end = int(input("Enter the end of range: "))  
 for num in range(start, end + 1): 
-      
-    # checking condition 
     if num % 2 != 0: 
         print(num, end = " ") 
